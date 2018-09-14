4f45cea1dbfb7fb2e2123e319f54925909db8995
Andre Marcelo-Tanner <andre@enthropia.com>
2018-09-14 16:31:37 +0800

    JS: update leap solution to make it simpler

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -4,22 +4,13 @@ class Year {
   }
 
   isLeap() {
-    let isLeapYear = false;
     let year = this.year;
 
-    if (year % 4 === 0) {
-      isLeapYear = true;
-
-      if (year % 100 === 0) {
-        isLeapYear = false;
-
-        if (year % 400 === 0) {
-          isLeapYear = true;
-        }
-      }
+    if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+      return true
     }
 
-    return isLeapYear;
+    return false;
   }
 }
 
