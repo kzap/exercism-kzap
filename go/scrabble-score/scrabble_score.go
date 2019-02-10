d8e345dcf2554cd6d8743d542b3f2ae6361a055f
Andre Marcelo-Tanner <andre@enthropia.com>
2019-02-10 18:50:23 +0800

    GO: update scrabble

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -7,14 +7,15 @@ import (
 
 var m = map[rune]int{
 	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
-	'D': 2, 'G': 2, 'B': 3, 'C': 3, 'M': 3, 'P': 3,
+	'D': 2, 'G': 2,
+	'B': 3, 'C': 3, 'M': 3, 'P': 3,
 	'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
 	'K': 5,
 	'J': 8, 'X': 8,
 	'Q': 10, 'Z': 10,
 }
 
-// Score takes in a word and returns its Scrabble score.
+// Score takes in a word and returns its Scrabble score using a map.
 func Score(word string) int {
 
 	if len(word) == 0 {
@@ -25,6 +26,36 @@ func Score(word string) int {
 	for _, r := range word {
 		if letterScore, exists := m[unicode.ToUpper(r)]; exists {
 			score += letterScore
+		}
+	}
+
+	return score
+}
+
+// ScoreSwitch takes in a word and returns its Scrabble score using a switch case.
+func ScoreSwitch(word string) int {
+
+	if len(word) == 0 {
+		return 0
+	}
+
+	var score int
+	for _, r := range word {
+		switch unicode.ToUpper(r) {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			score += 1
+		case 'D', 'G':
+			score += 2
+		case 'B', 'C', 'M', 'P':
+			score += 3
+		case 'F', 'H', 'V', 'W', 'Y':
+			score += 4
+		case 'K':
+			score += 5
+		case 'J', 'X':
+			score += 8
+		case 'Q', 'Z':
+			score += 10
 		}
 	}
 
--- go/scrabble-score/scrabble_score_test.go
+++ go/scrabble-score/scrabble_score_test.go
@@ -17,3 +17,19 @@ func BenchmarkScore(b *testing.B) {
 		}
 	}
 }
+
+func TestScoreSwitch(t *testing.T) {
+	for _, test := range scrabbleScoreTests {
+		if actual := ScoreSwitch(test.input); actual != test.expected {
+			t.Errorf("Score(%q) expected %d, Actual %d", test.input, test.expected, actual)
+		}
+	}
+}
+
+func BenchmarkScoreSwitch(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, test := range scrabbleScoreTests {
+			ScoreSwitch(test.input)
+		}
+	}
+}
