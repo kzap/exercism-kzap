1c2a8ab0b85a52a7ed416b75a043776dc97e2026
Andre Marcelo-Tanner <andre@enthropia.com>
2019-02-19 22:53:42 +0800

    GO: solution for isogram exercise

---
+++ go/isogram/isogram.go
@@ -0,0 +1,25 @@
+// Package isogram solves the Isogram exercise on Exercism's Go track.
+package isogram
+
+import (
+	"unicode"
+)
+
+// IsIsogram takes in a string and returns true if it is an Isogram or false if not.
+func IsIsogram(s string) bool {
+	m := make(map[rune]bool)
+
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			upperRune := unicode.ToUpper(r)
+
+			if _, exists := m[upperRune]; exists {
+				return false
+			}
+
+			m[upperRune] = true
+		}
+	}
+
+	return true
+}
